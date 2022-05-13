6b32fc6c295f70723584dbd3de41c225f64af45f
dosssman <dosssman@hotmail.fr>
2022-05-13 20:54:29 +0900

    Changed default Vultr region to Paris France (cdg)

--- create_pz_server.sh
+++ create_pz_server.sh
@@ -13,7 +13,7 @@ RESPONSE=$(curl "https://api.vultr.com/v2/instances" \
   -X POST \
   -H "Authorization: Bearer ${VULTR_API_KEY}" \
   -H "Content-Type: application/json" \
-  --data '{"region":"mad", "plan":"vc2-2c-4gb", "label":"PZSteamDediServerNode", "os_id":387, "sshkey_id":["a6fb92f8-b8d1-47c9-9511-8f2dc116714b","6bc9749a-ad09-4d35-add4-fc41afeb8341","164f25b6-55ff-44c8-a82c-f2bb784fd766"], "script_id": "e1e46aeb-b960-42b4-a63d-9a6a16d258e5","backups":"disabled","hostname":"PZSteamDediServerNode"}')
+  --data '{"region":"cdg", "plan":"vc2-2c-4gb", "label":"PZSteamDediServerNode", "os_id":387, "sshkey_id":["a6fb92f8-b8d1-47c9-9511-8f2dc116714b","6bc9749a-ad09-4d35-add4-fc41afeb8341","164f25b6-55ff-44c8-a82c-f2bb784fd766"], "script_id": "e1e46aeb-b960-42b4-a63d-9a6a16d258e5","backups":"disabled","hostname":"PZSteamDediServerNode"}')
 
 INSTANCE_ID=$(echo $RESPONSE | jq -r .instance | jq -r .id)
 echo $INSTANCE_ID > LAST_PZ_SERVER_SUBID
